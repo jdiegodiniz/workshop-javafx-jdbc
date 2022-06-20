362a50abef2de694a19d35a91116a6174351e110
Diego Diniz <diegodiniz90@hotmail.com>
2022-06-19 21:18:49 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+    private static Scene mainScene;
     @Override
     public void start(Stage primaryStage) {
         try{
@@ -16,7 +17,7 @@ public class Main extends Application {
             ScrollPane scrollPane = loader.load();
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
@@ -25,6 +26,9 @@ public class Main extends Application {
         catch (IOException e){
             e.printStackTrace();
         }
+    }
+    public static Scene getMainScene(){
+        return mainScene;
     }
 
     public static void main(String[] args) {
