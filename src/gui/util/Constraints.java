8aee254842f79d7dcd249294fcdc2bb0a31bb566
Diego Diniz <diegodiniz90@hotmail.com>
2022-06-20 14:11:04 -0300

    DepartmentForm controller

--- src/gui/util/Constraints.java
+++ src/gui/util/Constraints.java
@@ -3,6 +3,7 @@ package gui.util;
 import javafx.scene.control.TextField;
 
 public class Constraints {
+
     public static void setTextFieldInteger(TextField txt){
         txt.textProperty().addListener((obs, oldValue, newValue) -> {
             if (newValue != null && !newValue.matches("\\d*")){
@@ -11,7 +12,7 @@ public class Constraints {
         });
     }
 
-    private static void setTextFieldMaxLength(TextField txt, int max){
+    public static void setTextFieldMaxLength(TextField txt, int max){
         txt.textProperty().addListener((obs, oldValue, newValue) -> {
             if (newValue != null && newValue.length() > max){
                 txt.setText(oldValue);
